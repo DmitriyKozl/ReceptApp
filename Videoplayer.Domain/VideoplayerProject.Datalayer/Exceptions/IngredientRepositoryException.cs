55c982d9fe49cdf55b7b9ebe5afdfb9a4ec64211
Lynn Cornelis <lynn.cornelis2@gmail.com>
2023-11-20 16:49:15 +0100

    Added error handling in ingredientrepository

---
+++ Videoplayer.Domain/VideoplayerProject.Datalayer/Exceptions/IngredientRepositoryException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VideoplayerProject.Datalayer.Exceptions
+{
+    public class IngredientRepositoryException : Exception
+    {
+        public IngredientRepositoryException() { }
+
+        public IngredientRepositoryException(string message) : base(message) { }
+
+        public IngredientRepositoryException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
--- Videoplayer.Domain/VideoplayerProject.Datalayer/Repositories/IngredientRepository.cs
+++ Videoplayer.Domain/VideoplayerProject.Datalayer/Repositories/IngredientRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using VideoplayerProject.Datalayer.Data;
+using VideoplayerProject.Datalayer.Exceptions;
 using VideoplayerProject.Datalayer.Mappers;
 using VideoplayerProject.Domain.Exceptions;
 using VideoplayerProject.Domain.Interfaces;
@@ -17,55 +18,99 @@ public class IngredientRepository : IIngredientRepository {
 
     public List<DomainIngredient> GetIngredients(string? filter = null)
     {
-        var query = _context.Ingredients.AsQueryable();
-        
-        if (!string.IsNullOrEmpty(filter))
+        try
         {
-            query = query.Where(i => i.IngredientName.Contains(filter));
+            var query = _context.Ingredients.AsQueryable();
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                query = query.Where(i => i.IngredientName.Contains(filter));
+            }
+            
+            var dataIngredients = query.ToList();
+
+            return dataIngredients.Select(IngredientMapper.MapToDomainModel).ToList();
         }
-
-        var dataIngredients = query.ToList();
-
-        return dataIngredients.Select(IngredientMapper.MapToDomainModel).ToList();
+        catch (Exception ex)
+        {
+            throw new IngredientRepositoryException("Error in GetIngredients. ", ex);
+        }
     }
 
-
-
     public List<DomainIngredient> GetIngredientsFromRecipe(int recipeId) {
-        var dataIngredients = _context.RecipeIngredient
-            .Where(ri => ri.RecipeID == recipeId)
-            .Select(ri => ri.Ingredient)
-            .ToList();
+        try
+        {
+            var dataIngredients = _context.RecipeIngredient
+                .Where(ri => ri.RecipeID == recipeId)
+                .Select(ri => ri.Ingredient)
+                .ToList();
 
-        return dataIngredients.Select(IngredientMapper.MapToDomainModel).ToList();
+            return dataIngredients.Select(IngredientMapper.MapToDomainModel).ToList();
+        }
+        catch (Exception ex)
+        {
+            throw new IngredientRepositoryException("Error in GetIngredientsFromRecipe. ", ex);
+
+        }
     }
     
     public DomainIngredient GetIngredientById(int id) {
-        var dataIngredient = _context.Ingredients.Find(id);
-        return dataIngredient == null ? throw new IngredientException($"Invalid ingredient id or no ingredient exists with id:{id}") : IngredientMapper.MapToDomainModel(dataIngredient);
+        try
+        {
+            var dataIngredient = _context.Ingredients.Find(id);
+            return dataIngredient == null ? throw new IngredientException($"Invalid ingredient id or no ingredient exists with id:{id}") : IngredientMapper.MapToDomainModel(dataIngredient);
+        }
+        catch (Exception ex)
+        {
+            throw new IngredientRepositoryException("Error in GetIngredientById. ", ex);
+        }
     }
     
     public void CreateIngredient(DomainIngredient ingredient) {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            throw new IngredientRepositoryException("Error Creating ingredient. ", ex);
+
+        }
         var dataIngredient = IngredientMapper.MapToDataModel(ingredient);
         _context.Ingredients.Add(dataIngredient);
         _context.SaveChanges();
     }
     
     public void RemoveIngredient(int id) {
-        var dataIngredient = _context.Ingredients.Find(id);
-        if (dataIngredient == null) throw new IngredientException($"Invalid ingredient id or no ingredient exists with id:{id}");
+        try
+        {
+            var dataIngredient = _context.Ingredients.Find(id);
+            if (dataIngredient == null) throw new IngredientException($"Invalid ingredient id or no ingredient exists with id:{id}");
 
-        _context.Ingredients.Remove(dataIngredient);
-        _context.SaveChanges();
+            _context.Ingredients.Remove(dataIngredient);
+            _context.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new IngredientRepositoryException("Error removing ingredient. ", ex);
+
+        }
     }
 
     public void UpdateIngredient(DomainIngredient ingredient)
     {
-        var dataIngredient = IngredientMapper.MapToDataModel(ingredient);
+        try
+        {
+            var dataIngredient = IngredientMapper.MapToDataModel(ingredient);
 
-        if (dataIngredient == null) return;
+            if (dataIngredient == null) return;
 
-        _context.Ingredients.Update(dataIngredient);
-        _context.SaveChanges();
+            _context.Ingredients.Update(dataIngredient);
+            _context.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new IngredientRepositoryException("Error updating ingredient. ", ex);
+        }
     }
 }
